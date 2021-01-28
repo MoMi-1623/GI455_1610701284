06232050a7e742a9f2f0c483d05e702c1478f46b
LAPTOP-U9LG7RR3\HP_OMEN <KORBBOON.UDOM@BUMAI.NET>
2021-01-28 12:35:00 +0700

    Send Assignment Week1

---
+++ GI-455 Project/Assets/Script/TextData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextData : MonoBehaviour
+{
+    public string[] nameData;
+    [SerializeField] GameObject listText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(string data in nameData)
+        {
+            var listObj = Instantiate(listText,transform);
+            listObj.GetComponent<Text>().text = data;
+            listObj.transform.parent = gameObject.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

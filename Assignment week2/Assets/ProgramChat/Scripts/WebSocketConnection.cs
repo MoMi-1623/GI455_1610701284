a8713b826b3731442f46205d643a530099081b13
LAPTOP-U9LG7RR3\HP_OMEN <KORBBOON.UDOM@BUMAI.NET>
2021-02-05 19:12:04 +0700

    week2 programchat

---
+++ Assignment week2/Assets/ProgramChat/Scripts/WebSocketConnection.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using UnityEngine.UI;
+
+namespace ProgramChat
+{
+    public class WebSocketConnection : MonoBehaviour
+    {
+        private WebSocket webSocket;
+
+        public GameObject loginPanel;
+        public Text localInput;
+        public Text portInput;
+
+        public GameObject mainPanel;
+        public Text inputText;
+        public Text textPanel;
+
+        // Start is called before the first frame update
+        void Start()
+        {       
+            //webSocket.Send("I'm comming here");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //if(Input.GetKeyDown(KeyCode.Space))
+            //{
+            //    if(webSocket.ReadyState == WebSocketState.Open)
+            //    {
+            //        webSocket.Send("Random number : " + Random.Range(0, 999999));
+            //    }
+            //}
+        }
+        private void OnDestroy()
+        {
+            if(webSocket != null)
+            {
+                webSocket.Close();
+            }
+        }    
+
+        private void OnMessage(object sender, MessageEventArgs messageEventArgs)
+        {           
+                textPanel.text += messageEventArgs.Data + "\n";    
+        }
+        public void SendInputMessage()
+        {
+            if (webSocket.ReadyState == WebSocketState.Open)
+            {
+                webSocket.Send(inputText.text);
+            }
+
+        }
+        public void ConnectToChat()
+        {
+            if (localInput.text == "127.0.0.1" && portInput.text == "1623")
+            {
+                webSocket = new WebSocket("ws://127.0.0.1:1623/");
+                webSocket.Connect();
+                webSocket.OnMessage += OnMessage;
+                loginPanel.SetActive(false);
+                mainPanel.SetActive(true);
+            }
+        }
+    }
+}
---
+++ NodeJs/server.js
@@ -0,0 +1,76 @@
+const { CONNECTING } = require('ws');
+var websocket = require('ws');
+
+var callbackInitServer = ()=>
+{
+    console.log("server is running.");
+}
+
+var wss = new websocket.Server({port:1623} , callbackInitServer)
+
+var wslist = [];
+
+
+
+wss.on("connection",(ws)=>
+{   
+    wslist.push(ws);
+    for(var i = 0;i<wslist.length;i++)
+    {
+        if(wslist[i] == ws)
+        {
+            console.log("client " + i + " : connected");
+            break;
+        }
+    }
+    ws.on("message", (data)=>
+    {
+        for(var i = 0;i<wslist.length;i++)
+        {
+            if(wslist[i] != ws)
+            {
+                wslist[i].send(data + "                                               ");
+                continue;
+            }
+            if(wslist[i] == ws)
+            {
+                wslist[i].send(data);
+                console.log("sent from " + i + " " + data);
+                continue;
+            }
+        }
+    });
+
+    ws.on("close", ()=>
+    {
+        for(var i = 0;i<wslist.length;i++)
+        {
+            if(wslist[i] == ws)
+            {
+                console.log("client " + i + " disconnected");
+                wslist = RemoveArray(wslist,ws)
+                break;
+            }
+        }
+    });
+
+});
+
+
+function RemoveArray(arr, value)
+{
+    return arr.filter((element)=>
+    {
+        return element != value;
+    });
+}
+
+function Boardcast(data)
+{
+    for(var i = 0;i<wslist.length;i++)
+    {       
+            wslist[i].send(data);
+    }
+}
+
+
